c090e494d6cc38a167adeae214ea3ef1a932cd53
Jacob Roberts <jacobdr@gmail.com>
2025-05-05 00:01:11 +0000

    Fix devcontainer setup + add uv cache

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Since there are docker volume mounts we need to make sure the cache and venv directories are writable
+# by the non-root user
+sudo chmod -R 777 /home/vscode/.cache
+sudo chmod -R 777 /workspace/.venv
+
 uv venv
 uv run pip install --upgrade pip
 uv sync
